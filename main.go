3f756149b8158dcaeedf93d638f9b4c8eb9fc1bf
LiamWalshTech <admin@liamwalsh.tech>
2025-11-25 20:41:15 +0000

    CH1: L4 done

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()	
+		usersInput := scanner.Text()
+		usersInputClean := strings.ToLower(usersInput)
+		usersInputCleanWords := strings.Fields(usersInputClean)
+		fmt.Printf("Your command was: %v\n", usersInputCleanWords[0])
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
 }
